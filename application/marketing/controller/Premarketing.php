0e08ee0f397fb7cddf1c24ffcdf972e22042cbe7
hello <123456789@qq.com>
2019-10-24 11:21:12 +0800

    客户详情中客户课程无数据显示

--- application/marketing/controller/Premarketing.php
+++ application/marketing/controller/Premarketing.php
@@ -196,6 +196,12 @@ class Premarketing extends BasicAdmin
         $log = Db::name('SystemLog')->where('cid', '=', $id)->order('id', 'desc')->limit('20')->select();
         $introducer = Db::name('saas_customer_introducer')->where('customer_id', $id)->field('introducer_id')->find();
         $follow_status = Db::name('saas_customer_follow')->where('customer_id', '=', $id)->order('id desc')->find();
+        $courses = Db::name('saas_order o')
+            ->join('saas_order_log l', 'l.order_id=o.id', 'left')
+            ->join('saas_courses c', 'l.goods_id=c.id', 'left')
+            ->where('o.student_id', '=', $id)
+            ->field('o.id orderid, o.status ostatus, l.class_id, l.goods_id, c.title, l.price')
+            ->select();
         return view('', [
             'customer' => $customer,
             'title' => $this->title,
@@ -203,7 +209,8 @@ class Premarketing extends BasicAdmin
             'record' => $course_record,
             'log' => $log,
             'introducer' => $introducer,
-            'follow_status' => $follow_status
+            'follow_status' => $follow_status,
+            'courses' => $courses
         ]);
     }
 
